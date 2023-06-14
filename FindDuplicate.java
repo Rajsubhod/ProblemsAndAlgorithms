4755592623613ff55ba1db120ffc3d8d16cb6c5d
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-06-14 20:25:53 +0530

    Find Duplicate

---
+++ FindDuplicate.java
@@ -0,0 +1,40 @@
+//Find Duplicate elements in an array and return the number of duplicates
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,4,5,5,6,7,8,8,8,9};
+        DuplicateSorted.fun(arr);
+    }
+}
+class DuplicateSorted {
+
+    public static void fun(int[] arr) {
+        HashMap<Integer,Integer> map=new LinkedHashMap<>();
+        for(Integer num:arr){
+            if(map.containsKey(num)==false){
+                map.put(num,1);
+            }
+            else{
+                map.put(num, map.get(num)+1);
+            }
+        }
+        Iterator it =map.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry ele=(Map.Entry)it.next();
+            int value=(int)ele.getValue();
+            if(value>1){
+                System.out.print(ele.getKey()+" "+value);
+                System.out.println();
+            }
+        }
+    }
+
+}
+class DuplicateUnSorted {
+
+}
