e41e0e3005221ab2bd684316cebe2a63fa85de18
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-06-14 20:39:32 +0530

    Find Duplicate

--- FindDuplicate.java
+++ FindDuplicate.java
@@ -8,11 +8,33 @@ import java.util.Map;
 public class FindDuplicate {
     public static void main(String[] args) {
         int[] arr={1,2,3,4,4,5,5,6,7,8,8,8,9};
-        DuplicateSorted.fun(arr);
+        DuplicateSorted.fun2(arr);
     }
 }
 class DuplicateSorted {
 
+    public static void fun(int[] arr) {
+        int lastDuplicate=0;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1] && arr[i]!=lastDuplicate){
+                System.out.println(arr[i]);
+                lastDuplicate=arr[i];
+            }
+        }
+    }
+    public static void fun2(int[] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]==arr[i+1]){
+                int j=i+1;
+                while(arr[j]==arr[i]) j++;
+                System.out.println(arr[i]+" "+(j-i));
+                i=j-1;
+            }
+        }
+    }
+
+}
+class DuplicateUnSorted {
     public static void fun(int[] arr) {
         HashMap<Integer,Integer> map=new LinkedHashMap<>();
         for(Integer num:arr){
@@ -33,8 +55,4 @@ class DuplicateSorted {
             }
         }
     }
-
-}
-class DuplicateUnSorted {
-
 }
