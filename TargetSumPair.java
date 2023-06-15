d526615e014aa355315475b03c1662e181b7f5be
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-06-15 13:44:52 +0530

    Sum Pair

--- TargetSumPair.java
+++ TargetSumPair.java
@@ -7,8 +7,10 @@ public class TargetSumPair {
     public static void main(String[] args) {
         long startTime = System.nanoTime();
         int[] arr = {2, 5, 7, 8, 11, 3, 4};
+        int[] arr2={1,2,3,5,6,7,9,11,14};
         //System.out.println(Arrays.toString(new TargetSum().fun(arr)));
         System.out.println(Arrays.deepToString(fun2(arr)));
+        System.out.println(Arrays.deepToString(fun3(arr2)));
         long endTime = System.nanoTime();
         System.out.println("Took "+(endTime - startTime) + " ns");
         startTime = System.nanoTime(); 
@@ -41,6 +43,26 @@ public class TargetSumPair {
                list.add(new int[]{arr[i], 7-arr[i]});
             }
             hash[arr[i]]++;
+        }
+        return list.toArray(new int[0][]);
+    }
+    //Forted sorted array can use two pointer
+    public static int[][] fun3(int[] arr){
+        ArrayList<int[]> list = new ArrayList<>();
+        int i=0,j=arr.length-1;
+        while(j!=i){
+            if(arr[i]+arr[j]==7){
+                list.add(new int[]{arr[i],arr[j]});
+                i++;
+                j--;
+            }
+            else if(arr[i]+arr[j]>7){
+                j--;
+            }
+            else{
+                i++;
+            }
+
         }
         return list.toArray(new int[0][]);
     }
