5f4514e73fcf8d77135a74821b9e93c92550a719
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-06-17 20:10:13 +0530

    GCD calculation

--- GCD.java
+++ GCD.java
@@ -1,14 +1,25 @@
 //Given two numbers x and y find the greatest common divisor of x and y
 //constraints 0<= x,y <=1e6(10^6)
 public class GCD {
-    //  force approach 
-    //TIme complexity O(min(x,y)) ---- useless approach
+    //Euclid's algorithm approach -> GCD(x,y)=GCD(y,x%y) & GCD(x,0)=x
+
+    private static int gcd(int x, int y) {
+        if(y==0){
+            return x;
+        }
+        return gcd(y, x%y);
+    }
+    // Long division Iterative approach 
+    //TIme complexity O(min(x,y)) ---- a bit better still not good approach
     private static int fun2(int x, int y) {
         int divisor=Math.min(x, y);
         int dividend=Math.max(x,y);
-        int remainder=-1;
+        /* It's not necessary to intialize the divisor and dividend as we can simply write x%y
+           which will work for x<y but even if x>y it would still work but now it will do 1 extra
+           division in which the firat division it will swap the places of the two (x,y) 
+         */
         while(dividend%divisor!=0){
-            remainder=dividend%divisor;
+           int remainder=dividend%divisor;
             dividend=divisor;
             divisor=remainder;
         }
@@ -30,7 +41,9 @@ public class GCD {
         int x2=6;
         int y2=18;
         System.out.println(fun(x,y));
-        System.out.println(fun2(x2,y2));
         System.out.println(fun(x2,y2));
+        System.out.println(fun2(x2,y2));
+        System.out.println(gcd(x,y));
+        System.out.println(gcd(x2,y2));
     }    
 }
