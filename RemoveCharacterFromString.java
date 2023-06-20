ac2cc7d6588ea65dca90c93c1049ff4993734c34
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-06-20 23:56:08 +0530

    remove occurances of a character from a string

--- RemoveCharacterFromString.java
+++ RemoveCharacterFromString.java
@@ -1,11 +1,20 @@
 //Remove occerances of an character from a string
 public class RemoveCharacterFromString{
-    public static void main(String[] args) {
-        String str="Saaasas";
-        System.out.println(fun(str));
+    //Using recursion
+    private static String funR(String str,int i) {
+        if(i==str.length()){
+            return "";
+        }
+        String ans=funR(str, i+1);
+        if(str.charAt(i)!='a'){
+            return str.charAt(i)+ans;
+        }
+        else{
+            return ans;
+        }
     }
-
-    private static String fun(String str) {
+    //Using iteration
+    private static String funI(String str) {
         String ans="";
         for(int c=0;c<str.length();c++){
             if(str.charAt(c)!='a'){
@@ -14,4 +23,9 @@ public class RemoveCharacterFromString{
         }
         return ans;
     }
+      public static void main(String[] args) {
+        String str="Saaasas";
+        System.out.println(funR(str,0));
+    }
+
 }
