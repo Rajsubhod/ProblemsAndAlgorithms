ab3d82d183a531773f8f67a5d73ae13fcd8ccdf2
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-06-23 21:29:21 +0530

    print sum of all subsets of an array

--- SumOfSubsequences.java
+++ SumOfSubsequences.java
@@ -7,18 +7,18 @@ public class SumOfSubsequences {
         int[] arr = {2,4,5};
         fun(arr,0);
     }
-
-    private static void fun(int[] arr,int rem) {
+    //Time complexity --- O(2^n)
+    private static void fun(int[] arr,int sum) {
         if(arr.length==0){
-            System.out.println(rem);
+            System.out.println(sum);
             return;
         }
         int curr = arr[0];
         arr = Arrays.copyOfRange(arr, 1, arr.length);
          //Take the curr into account
-        fun(arr,rem+curr);
+        fun(arr,sum+curr);
         //Do not take curr into account
-        fun(arr,rem);
+        fun(arr,sum);
     }
 }
 
