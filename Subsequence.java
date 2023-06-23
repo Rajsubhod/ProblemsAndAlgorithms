76df214d1463c81b59320a70f6f13039d3a2f8db
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-06-23 21:07:05 +0530

    print all sebsequences

--- Subsequence.java
+++ Subsequence.java
@@ -8,12 +8,13 @@ public class Subsequence {
         String str="abc";
         ArrayList<String> list = ArrayListSubsequence.fun(str);
         System.out.println(list);
+        PrintSubsequence.fun(str, "");
     }
 }
 
 //Given a string return a arraylist containing all subsequences of the string
 class ArrayListSubsequence {
-    static ArrayList<String> fun(String str) {
+    public static ArrayList<String> fun(String str) {
         ArrayList<String> ans = new ArrayList<>();
 
         if(str.length()==0){
@@ -40,28 +41,17 @@ class ArrayListSubsequence {
 
 //Given a string print all subsequences of the string
 class PrintSubsequence {
-    static ArrayList<String> fun(String str) {
-        ArrayList<String> ans = new ArrayList<>();
-
+    public static void fun(String str,String remString) {
         if(str.length()==0){
-            ans.add("");
-            return ans;
+            System.out.print(remString+ " ");
+            return;
         }
-
-        char c = str.charAt(0);
-        ArrayList<String> smallans = fun(str.substring(1));
-
-        for(String ss:smallans){
-            ans.add(ss);
-            ans.add(c+ss);
-        }
-        //this uses default comparator which compares via character order
-        //Collections.sort(ans);
-        //This does sorting via custom comparator that compares string length and then via character order
-        Comparator<String> comparator = Comparator.comparingInt(String::length)
-                .thenComparing(Comparator.naturalOrder());
-        Collections.sort(ans, comparator);
-        return ans;
+        char curr = str.charAt(0);
+        String remeningString = str.substring(1);
+        //Take the curr into account
+        fun(remeningString,remString+curr);
+        //Do not take curr into account
+        fun(remeningString,remString);
     }
 }
 
