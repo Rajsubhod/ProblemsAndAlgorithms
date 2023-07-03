864b2ce7ced36b5c04c1a449dc373278b06740a6
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-07-03 22:05:26 +0530

    searching problem 3 : Finding index of min element in rotated sorted array

---
+++ SearchProblem3.java
@@ -0,0 +1,42 @@
+//Given a rotated sorted array find the minimum value of the array
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class SearchProblem3 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,5,7,8,9};
+        Random random = new Random();
+        //random.setSeed(1);
+        int x = random.nextInt(10);
+        new RotateArray().funR(arr,x);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(fun(arr));
+    }
+    //Time Complexity --- O(logn)
+    private static int fun(int[] arr) {
+        int low = 0;
+        int high = arr.length-1;
+        int min = Integer.MAX_VALUE;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            /*
+                if(arr[mid]>arr[high]){
+                    low = mid + 1;
+                }
+                else if(arr[mid]<=arr[high]){
+                    min = mid;
+                    high = mid-1;
+                }
+            */
+            if(arr[mid]<=arr[high]){
+                if(arr[mid]<min) min = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return min;
+    }
+}
