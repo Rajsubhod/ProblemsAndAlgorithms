c3a637ad1027b89a9b6d2fd8de40b3b7fb3e885a
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-07-04 19:41:27 +0530

    searching problem 9 : Finding peak element II

--- SearchProblem9.java
+++ SearchProblem9.java
@@ -13,7 +13,7 @@ public class SearchProblem9 {
         int ans=-1;
         while(low<=high){
             int mid = low+(high-low)/2;
-            if((mid==0 || arr[mid-1]<arr[mid]) && (mid== arr.length-1 || arr[mid]>arr[mid+1])){
+            if((mid==0 || arr[mid-1]<arr[mid]) && (mid==arr.length-1 || arr[mid]>arr[mid+1])){
                 return mid;
             }
             else if(arr[mid]<arr[mid+1]){ //Uphill
