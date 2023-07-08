59131dc9d3f7b4ebecc1866dc09ad41a4c6165ec
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-07-08 21:22:51 +0530

    Search Problem 2 : Finding the square root of a given number not using library funtions

--- SearchProblem2.java
+++ SearchProblem2.java
@@ -2,7 +2,9 @@
 public class SearchProblem2 {
     public static void main(String[] args) {
         int x = 24;
+        int y= 2147395599;
         System.out.println(fun(x));
+        System.out.println(funL(y));
     }
     //Time Complexity --- O(logn)
     private static int fun(int x) {
@@ -19,6 +21,26 @@ public class SearchProblem2 {
                 high = mid-1;
             }
             else{
+                low = mid+1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+    // A revised method that handles large int value like 2147395599
+    public static int funL(int x){
+        int low = 0, high = x;
+        int ans = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            long val = (long) mid*mid;
+            if(val==x){
+                return mid;
+            }
+            else if(val>x) {
+                high = mid-1;
+            }
+            else {
                 low = mid+1;
                 ans = mid;
             }
