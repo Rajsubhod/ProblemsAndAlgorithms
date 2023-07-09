bbf18a4d6038ba2ba5e8029389da1e4b003be361
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-07-09 20:37:35 +0530

    find the index of the targget in array if not present return the index where it could be positioned

---
+++ SearchInsertPosition.java
@@ -0,0 +1,43 @@
+/*
+    You are given a sorted array arr of distinct values and a target value x. You need to search for the index of the target value in the array.
+
+    If the value is present in the array, then return its index. Otherwise, determine the index where it would be inserted in the array while maintaining the sorted order.
+    Example 1:
+    Input Format: arr[] = {1,2,4,7}, x = 6
+    Result: 3
+    Explanation: 6 is not present in the array. So, if we will insert 6 in the 3rd index(0-based indexing), the array will still be sorted. {1,2,4,6,7}.
+
+    Example 2:
+    Input Format: arr[] = {1,2,4,7}, x = 2
+    Result: 1
+    Explanation: 2 is present in the array and so we will return its index i.e. 1.
+ */
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] arr = {1};
+        int x = -1;
+        System.out.println(search(arr,x));
+    }
+
+    private static int search(int[] arr, int x) {
+        int n = arr.length;
+        int low=0;
+        int high=n-1;
+        int ans=0;
+        //here end value of ans and low are same so there was no need for creating ans
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==x){
+                return mid;
+            }
+            else if(arr[mid]<x){
+                ans=mid+1;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }       
+        }
+        return ans;
+    }
+}
