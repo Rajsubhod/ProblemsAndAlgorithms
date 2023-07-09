4877a1272f015463c0827d306f10784053c75e70
Rajdeep Mukherjee <rajdeepmukherjee2002@gmail.com>
2023-07-09 20:38:33 +0530

    Find the upper and lower bound of target in array

--- ImplementUpperLowerBound.java
+++ ImplementUpperLowerBound.java
@@ -15,43 +15,48 @@ import java.util.Arrays;
 
 public class ImplementUpperLowerBound {
     public static void main(String[] args) {
-        int[] arr = {1,2,2,3};
-        int x = 4;
+        int[] arr = {1};
+        int x = 1;
         System.out.println(Arrays.toString(upperlowerBond(arr,x)));
     }
     //Time Complexity --- O((logn)^2)
-    private static int[] upperlowerBond(int[] arr, int x) {
-        int st = 0;
-        int end = arr.length-1;
-        int low=-1,high=-1;
-        //Finding low
-        while(st<=end){
-            int mid = st+(end-st)/2;
-            if(arr[mid]<=x){
-                low=mid;
-                st=mid+1;
+    private static int[] upperlowerBond(int[] arr, int i) {
+        if(arr.length<1){
+            return new int[]{-1,-1};
+        }
+        int l=0;
+        int h=arr.length-1;
+        int occuranceL = -1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]==i){
+                occuranceL = mid;
+                h = mid-1;
+            }
+            else if(arr[mid]>i){
+                h=mid-1;
             }
             else{
-                end=mid-1;
+                l=mid+1;
             }
         }
-        st=0;
-        end=arr.length-1;
-        //Fiding high
-        while(st<=end){
-            int mid = st+(end-st)/2;
-            if(arr[mid]>x){
-                high=mid-1;
-                end=mid-1;
+        l=0;
+        h=arr.length-1;
+        int occuranceH = -1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]==i){
+                occuranceH = mid;
+                l = mid+1;
+            }
+            else if(arr[mid]>i){
+                h=mid-1;
             }
             else{
-                st=mid+1;
+                l=mid+1;
             }
         }
-        int[] ans = {low,high};
-        if(ans[0]!=x){
-            ans[0]=-1;
-        }
+        int[] ans = {occuranceL,occuranceH};
         return ans;
     }
 }
